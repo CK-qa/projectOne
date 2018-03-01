fcbb3a8dcecfab214113dae4ad32377ab1b3665d
Victoria Bozhko <viktoria.bozhko@jetbrains.com>
2018-03-01 13:47:23 +0300

    commit

---
+++ src/main/java/ClassA.java
@@ -0,0 +1,16 @@
+public class ClassA {
+
+    String s = "String";
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public Integer foo() {
+        return 1;
+    }
+}
